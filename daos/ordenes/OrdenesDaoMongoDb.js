c89a604a43466fd479b17b47f32fd8b2cc378971
sebaortega <ortegamolarosebastian@gmail.com>
2023-03-02 18:39:05 -0300

    Cuando hago el post a carrito la orden no se esta generando

--- daos/ordenes/OrdenesDaoMongoDb.js
+++ daos/ordenes/OrdenesDaoMongoDb.js
@@ -1,32 +1,32 @@
 const  {ContenedorMongoDb}  = require("../../contenedores/mongoContain");
+const mongoose = require('mongoose');
 
 class DAOOrdenesMongo extends ContenedorMongoDb {
-  constructor() {    
-    super("Orden", 
-    { 
-      userId: { type: String, required: true },
-      productos: [
-        {
-          productosId: {
-            type: String,
+  constructor() {
+    super("Orden",
+      {
+        id_usuario: { type: String, required: true },
+        id_carrito: { type: mongoose.Schema.Types.ObjectId, ref: "Carrito", required: true },
+        productos: [
+          {
+            id_prod: { type: String, required: true },
+            nombre: { type: String, required: true },
+            descripcion: { type: String, required: true },
+            codigo: { type: String, required: true },
+            imgUrl: { type: String, required: true },
+            precio: { type: Number, required: true },
+            cantidad: { type: Number, required: true },
+            categoria: { type: String, required: true },
           },
-          cantidad: {
-            type: Number,
-            default: 1,
-          },
-          categoria: {
-            type: String,          
-          },
-        },
-      ],
-      totalCompra: { type: Number, required: true },
-      direccion: { type: Object, required: true },
-      estado: { type: String, default: "pendiente" },
-    },
-  { timestamp: Date 
-    });
+        ],
+        totalCompra: { type: Number, required: true },
+        direccion: { type: Object, required: true },
+        estado: { type: String, default: "pendiente" },
+        timestamp: { type: Date, default: Date.now }
+      }
+    );
   }
-  
 }
+
 
 module.exports = DAOOrdenesMongo;
